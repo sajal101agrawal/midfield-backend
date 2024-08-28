4c3403307c80d1406c817297958ff38228f1c560
riken-sajal <riken@sajaltech.com>
2024-08-28 13:14:32 +0530

    change url

--- google_auth/urls.py
+++ google_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sign_in, name='sign_in'),
+    path('auth/sign-in', views.sign_in, name='sign_in'),
     path('sign-out', views.sign_out, name='sign_out'),
     path('auth-receiver/', views.auth_receiver, name='auth_receiver'),
     path('dashboard', views.dashboard.as_view(), name='dashboard'),
