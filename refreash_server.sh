4cf8f0dcc27ceb4f4f37909e14c48092ee3f78d9
riken-sajal <riken@sajaltech.com>
2024-08-29 17:19:15 +0530

    refreash server

---
+++ refreash_server.sh
@@ -0,0 +1,8 @@
+APP_DIR="/home/ubuntu/midfield-backend"
+VENV_DIR="$APP_DIR/env"
+
+source $VENV_DIR/bin/activate
+python manage.py makemigrations
+python manage.py migrate
+sudo systemctl restart gunicorn
+sudo systemctl restart nginx
