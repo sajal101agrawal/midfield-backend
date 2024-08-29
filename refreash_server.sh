0922d23d88d8c83ac5b6e69ed8f850d60789f3f6
riken-sajal <riken@sajaltech.com>
2024-08-29 17:20:24 +0530

    dsdsd

--- refreash_server.sh
+++ refreash_server.sh
@@ -2,6 +2,7 @@ APP_DIR="/home/ubuntu/midfield-backend"
 VENV_DIR="$APP_DIR/env"
 
 source $VENV_DIR/bin/activate
+git pull
 python manage.py makemigrations
 python manage.py migrate
 sudo systemctl restart gunicorn
